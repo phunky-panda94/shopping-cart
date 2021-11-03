d96084f9829198fd1dd90baa1f3db7f526001645
phunky-panda94 <victor.nim94@outlook.com>
2021-11-03 14:22:50 +1100

    initial commit

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from './App';
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
