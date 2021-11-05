d493c6ec4f41ebcd3e10bdd151a078bc09f7c5cb
phunky-panda94 <victor.nim94@outlook.com>
2021-11-05 15:33:10 +1100

    refactored cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,27 +3,20 @@ import { useEffect } from 'react';
 
 function Cart(props) {
 
-    const { items, setItems } = props;
-
-    // fetch shopping cart items upon mounting
-    useEffect(() => {
-        if (localStorage.getItem('cart') != null) {
-            setItems(new Map(Object.entries(JSON.parse(localStorage.getItem('cart')))));
-        }
-    }, [setItems]);
+    const { cart, setCart } = props;
 
     function removeFromCart(itemId) {
         
         if (window.confirm('Are you sure?')) {
         
             // delete item from cart map
-            items.delete(itemId);
-            let updatedItems = new Map(props.items);
+            cart.delete(itemId);
+            let updatedCart = new Map(cart);
 
-            setItems(updatedItems);
+            setCart(updatedCart);
 
             // update local storage
-            localStorage.setItem('cart', JSON.stringify(Object.fromEntries(updatedItems)));
+            localStorage.setItem('cart', JSON.stringify(Object.fromEntries(updatedCart)));
             
         }
 
@@ -35,14 +28,14 @@ function Cart(props) {
                 <h1>My Shopping Cart</h1>
             </div>
             <div className="box">
-                {items.size === 0 ? <h3>You don't have any items in your shopping cart!</h3> :
-                    Array.from(items.values()).map(item => {
+                {cart.size === 0 ? <h3>You don't have any items in your shopping cart!</h3> :
+                    Array.from(cart.values()).map(item => {
                         return <Item key={item.details.name} product={item} removeFromCart={removeFromCart}/>
                     })
                 }
             </div>
             <div className="box flex flex-jc-fe">
-                <button className="bg-dark-green" disabled={items.size === 0}>Checkout</button>
+                <button className="bg-dark-green" disabled={cart.size === 0}>Checkout</button>
             </div>
         </div>
     )
